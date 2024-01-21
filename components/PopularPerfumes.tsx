7f1610ce73c42a235e88313f91cba126705a9c29
Phoebe Chang <phoebe03111@gmail.com>
2024-01-20 20:45:47 -0800

    Create PopularPerfumes component

--- components/PopularPerfumes.tsx
+++ components/PopularPerfumes.tsx
@@ -1,6 +1,7 @@
 import prisma from "@/lib/prisma";
+import Link from "next/link";
 
-const getProduct = async () => {
+const getProducts = async () => {
   const data = await prisma.product.findMany({
     include: {
       categories: {
@@ -14,10 +15,24 @@ const getProduct = async () => {
 };
 
 const PopularPerfumes = async () => {
-  const data = await getProduct();
-  console.log(data);
+  const products = await getProducts();
+  console.log(products);
 
-  return <div>PopularPerfumes</div>;
+  return (
+    <section className="py-24">
+      <div className="container mx-auto">
+        <h2 className="text-center">Best Sellers</h2>
+        <p className="text-center mb-[30px]">
+          Your Favorite Perfumes In One Place.
+        </p>
+
+        {/* PopularPerfumesCarousel */}
+
+        <Link href="/our-bikes">
+          <button className="btn btn-accent mx-auto">See all perfumes</button>
+        </Link>
+      </div>
+    </section>
+  );
 };
-
 export default PopularPerfumes;
